b972180229e03a5757ec112bf273ea60d7268638
AiAoHaru <3110aiaoharu@gmail.com>
2020-08-27 16:29:59 +0900

    サインアップの画面に遷移するように実装 & nicknameをDBに保存できるように設定
    ・ログインしていないユーザーをサインアップの画面に促す設定
    ・nicknameをDBに保存できるように設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user! #ログイン済みか判断し、ログインしていないユーザーに対して、サインアップ画面に遷移させるための記述。
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname])
+  end
 end
