bdae690762a4066ae19a8e911d4661cba9fd6dba
AiAoHaru <3110aiaoharu@gmail.com>
2020-08-28 16:29:09 +0900

    任意指定カラムの許容設定 及び ログイン判断によるサインアップ画遷移設定
    ・ログイン済みか判断し、ログインしていないユーザーに対して、サインアップ画面に遷移させるための記述
    before_action :authenticate_user!, except: [:index]
    
    ・任意で指定したカラムについてのDBへ保存許容の設定devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :family_name, :last_name, :family_name_reading, :last_name_reading, :birth_date])

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user! #ログイン済みか判断し、ログインしていないユーザーに対して、サインアップ画面に遷移させるための記述。
+  before_action :authenticate_user!, except: [:index] #ログイン済みか判断し、ログインしていないユーザーに対して、サインアップ画面に遷移させるための記述。
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :family_name, :last_name, :family_name_reading, :last_name_reading, :birth_date])
   end
 end
