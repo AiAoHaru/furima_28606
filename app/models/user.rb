06883c5a7ecc01e01caf50513e4a64ab0c37e336
AiAoHaru <3110aiaoharu@gmail.com>
2020-08-29 12:56:03 +0900

    バリデーションとエラーメッセージの修正
    ・エラーメッセージ
    →表示される文章の修正
    ・バリデーション
    →nickname に対して 一意性を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,14 +16,14 @@ class User < ApplicationRecord
   VALID_NAME_READING_REGEX = /\A[ァ-ヶー－]+\z/.freeze
 
 # エラーメッセージ
-  ERROR_MESSAGE_EMAIL = "is invalid. Please input email correctly."   # （メールアドレスを正しく入力してください）"
-  ERROR_MESSAGE_PASSWORD = "is invalid. Please input half-width characters."    # （パスワードは、半角英数字で入力してください）"
-  ERROR_MESSAGE_NAME = "is invalid. Please input full-width characters."    # （お名前は、全角で入力してください）"
-  ERROR_MESSAGE_NAME_READING = "is invalid. Please input full-width katakana characters."   # （お名前カナは、全角カナで入力してください）"
+  ERROR_MESSAGE_EMAIL = "is invalid. input email correctly."   # （メールアドレスを正しく入力してください）"
+  ERROR_MESSAGE_PASSWORD = "is invalid. input half-width characters."    # （パスワードは、半角英数字で入力してください）"
+  ERROR_MESSAGE_NAME = "is invalid. input full-width characters."    # （お名前は、全角で入力してください）"
+  ERROR_MESSAGE_NAME_READING = "is invalid. input full-width katakana characters."   # （お名前カナは、全角カナで入力してください）"
 
 # バリデーション
   with_options presence: true do    # with_optionsで全てのバリデーションに共通したオプション（presence: true）をdo~endで指定したものに付ける
-    validates :nickname
+    validates :nickname, uniqueness: true   # 一意性を追加
     validates :email, uniqueness: true, format: { with: VALID_EMAIL_REGEX, message: ERROR_MESSAGE_EMAIL }
     validates :password, format: { with: VALID_PASSWORD_REGEX, message: ERROR_MESSAGE_PASSWORD }
     validates :family_name, format: { with: VALID_NAME_REGEX, message: ERROR_MESSAGE_NAME }
