530f06591d12d80777279c08112bb6dcebc1f104
AiAoHaru <3110aiaoharu@gmail.com>
2020-08-29 17:09:00 +0900

    エラーメッセージの並び順を変更 & コメントアウト追加
    ◉今回の実装では、エラーメッセージの並び順に影響する
    ※理由：railsではコードが上から順に読まれるため
    
    deviseについて標準であるものについては、上の方で以下のように 「 :validatable 」とバリデーションの設定をしているため。
    
    devise :database_authenticatable, :registerable,
             :recoverable, :rememberable, :validatable

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,8 @@ class User < ApplicationRecord
 # 正規表現
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
-  VALID_NAME_READING_REGEX = /\A[ァ-ヶー－]+\z/.freeze
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze   # 全角ひらがな、全角カタカナ、漢字
+  VALID_NAME_READING_REGEX = /\A[ァ-ヶー－]+\z/.freeze    # 全角カタカナ
 
 # エラーメッセージ
   ERROR_MESSAGE_EMAIL = "is invalid. Input email correctly."   # （メールアドレスを正しく入力してください）"
@@ -21,14 +21,14 @@ class User < ApplicationRecord
   ERROR_MESSAGE_NAME = "is invalid. Input Full-width characters."    # （お名前は、全角で入力してください）"
   ERROR_MESSAGE_NAME_READING = "is invalid. Input Full-width katakana characters."   # （お名前カナは、全角カナで入力してください）"
 
-# バリデーション
+# バリデーション（今回の実装では、コードの並び順がエラーメッセージの並び順に影響する）
   with_options presence: true do    # with_optionsで全てのバリデーションに共通したオプション（presence: true）をdo~endで指定したものに付ける
     validates :nickname, uniqueness: true   # 一意性を追加
     validates :email, uniqueness: true, format: { with: VALID_EMAIL_REGEX, message: ERROR_MESSAGE_EMAIL }
     validates :password, format: { with: VALID_PASSWORD_REGEX, message: ERROR_MESSAGE_PASSWORD }
     validates :family_name, format: { with: VALID_NAME_REGEX, message: ERROR_MESSAGE_NAME }
-    validates :last_name, format: { with: VALID_NAME_REGEX, message: ERROR_MESSAGE_NAME }
     validates :family_name_reading, format: { with: VALID_NAME_READING_REGEX, message: ERROR_MESSAGE_NAME_READING }
+    validates :last_name, format: { with: VALID_NAME_REGEX, message: ERROR_MESSAGE_NAME }
     validates :last_name_reading, format: { with: VALID_NAME_READING_REGEX, message: ERROR_MESSAGE_NAME_READING }
     validates :birth_date
   end
