4fdc35ae06f7f1da5de631a647a37096124d5b6e
AiAoHaru <3110aiaoharu@gmail.com>
2020-09-01 20:16:02 +0900

    Basic認証を導入（仮ユーザー名＆仮パスワード）
    環境変数設定前（仮設定）
    仮ユーザー名：admin
    仮パスワード：password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
   before_action :authenticate_user!, except: [:index] #ログイン済みか判断し、ログインしていないユーザーに対して、サインアップ画面に遷移させるための記述。
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -6,5 +8,13 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :family_name, :last_name, :family_name_reading, :last_name_reading, :birth_date])
+  end
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == 'password'
+    end
   end
 end
