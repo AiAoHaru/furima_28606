29736c94b1c729cd7ad6b4dac79b00879b081390
AiAoHaru <3110aiaoharu@gmail.com>
2020-09-03 19:39:59 +0900

    set_itemメソッド及びbefore_actionの実装
    set_itemメソッドをprivate以下に作成し、before_actionでの呼び出しに修正。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   
+  before_action :set_item, only: [:show, :destroy]
+
   def index
   end
 
@@ -18,8 +20,7 @@ class ItemsController < ApplicationController
   end
   
   def destroy
-    item = Item.find(params[:id])
-    if item.destroy   # 削除が完了した時
+    if @item.destroy   # 削除が完了した時
       return redirect_to root_path
     else   # 削除に失敗した時
       redirect_to  :show
@@ -33,13 +34,16 @@ class ItemsController < ApplicationController
   end
   
   def show
-    @item = Item.find(params[:id])
   end
 
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :postage_payer_id, :prefecture_id,:handling_time_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
