b5efa53591c14c49763268ef2fcbdf7eb9bca21c
AiAoHaru <3110aiaoharu@gmail.com>
2020-09-09 11:18:39 +0900

    不要なインデントの削除

--- app/models/purchaser.rb
+++ app/models/purchaser.rb
@@ -4,5 +4,4 @@ class Purchaser < ApplicationRecord
   belongs_to :item
   has_one :address, dependent: :destroy   # 購入済みの商品（Item）について、出品者が削除した場合に、購入者情報（Purchaser）に紐付いている配送先情報（Address）も一緒に削除させる設定
 
-  
 end
