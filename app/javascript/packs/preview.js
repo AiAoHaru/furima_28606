74156b6df83aeb0ce9d812c0641745f4136a0d54
AiAoHaru <3110aiaoharu@gmail.com>
2020-09-21 13:26:14 +0900

    リファクタリング
    createImageHTMLという名前の関数を自作します。この関数の役割は「選択した画像を表示する」こと

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,15 +1,8 @@
 if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
   document.addEventListener('DOMContentLoaded', function(){
     const ImageList = document.getElementById('image-list');
-    document.getElementById('item-image').addEventListener('change', function(e){
-      // 画像が表示されている場合のみ、すでに存在している画像を削除する
-      const imageContent = document.querySelector('img');
-      if (imageContent){
-        imageContent.remove();
-      }
-      const file = e.target.files[0];
-      const blob = window.URL.createObjectURL(file);
-
+    // 選択した画像を表示する関数
+    const createImageHTML = (blob) => {
       // 画像を表示するためのdiv要素を生成
       const imageElement = document.createElement('div');
 
@@ -20,6 +13,19 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
       // 生成したHTMLの要素をブラウザに表示させる
       imageElement.appendChild(blobImage);
       ImageList.appendChild(imageElement);
+    };
+
+    document.getElementById('item-image').addEventListener('change', function(e){
+      // 画像が表示されている場合のみ、すでに存在している画像を削除する
+      const imageContent = document.querySelector('img');
+      if (imageContent){
+        imageContent.remove();
+      }
+
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+
+      createImageHTML(blob);
   });
 });
 }
