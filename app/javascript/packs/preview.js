8e41ee375d476a82e1acd9b9b2714a82aa0994fa
AiAoHaru <3110aiaoharu@gmail.com>
2020-09-21 13:22:39 +0900

    TOP画面に表示されているエラーを解決
    JavaScriptの実行条件分岐を指定

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,3 +1,4 @@
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
 document.addEventListener('DOMContentLoaded', function(){
   const ImageList = document.getElementById('image-list');
   document.getElementById('item-image').addEventListener('change', function(e){
@@ -16,3 +17,4 @@ document.addEventListener('DOMContentLoaded', function(){
     ImageList.appendChild(imageElement);
   });
 });
+}
