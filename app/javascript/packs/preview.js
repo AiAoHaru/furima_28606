be1ef3045c15660498d9146777cf486751aebf51
AiAoHaru <3110aiaoharu@gmail.com>
2020-09-22 15:29:15 +0900

    選択したアップロード画像が複数プレビュー表示される問題の改善

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -11,15 +11,15 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
       blobImage.setAttribute('src', blob);
 
       // 生成したHTMLの要素をブラウザに表示させる
-      imageElement.appendChild(blobImage).setAttribute("class", "resize_image");;
+      imageElement.appendChild(blobImage).setAttribute("class", "resize_image");
       ImageList.appendChild(imageElement);
     };
 
     document.getElementById('item-image').addEventListener('change', function(e){
       // 画像が表示されている場合のみ、すでに存在している画像を削除する
       const imageContent = document.querySelector('img');
-      if (imageContent[1]){
-        imageContent[1].remove();
+      if (imageContent){
+        imageContent.remove();
       }
 
       const file = e.target.files[0];
